e94a51024ed17b1d837b6b6b35e92b47a9b26ebd
Victor LaBrie <vplabrie@gmail.com>
2022-11-27 02:31:54 -0600

    Begin.

---
+++ PA4.py
@@ -0,0 +1,8 @@
+#Program Name: Term-Document Co-Occurrence
+#Author: Victor Paul LaBrie
+#Date: November 27th, 2022
+#Problem: Write a program in the language of your 
+# choice that creates a term by document co-occurrence 
+# matrix for a collection of books, and then measures 
+# the pairwise cosine similarity between every pair of 
+# these books.
