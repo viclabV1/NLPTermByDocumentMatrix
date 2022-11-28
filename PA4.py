3f318716880998b73f12364dad5496d12fe5ab5c
Victor LaBrie <vplabrie@gmail.com>
2022-11-28 00:36:30 -0600

    Outputs list of cosine similarities.

--- PA4.py
+++ PA4.py
@@ -8,6 +8,7 @@
 # these books.
 import sys
 import re
+import numpy as np
 #Get arguments
 lower = sys.argv[1]
 upper = sys.argv[1]
@@ -16,27 +17,73 @@ print(textFiles)
 
 def main():
     #First, construct a corpus vocab
-    corpusDict = {}
+    corpusList = []
     corpusTokens = 0
 
+    #Create a list of all words in the corpus
     for textFile in textFiles:
         thisFile = open(textFile)
         thisText = thisFile.read().lower()
+        thisFile.close()
         #remove all but the words
         cleanedText = re.sub(r"[^a-zA-Z0-9]", " ", thisText)
-        textWords = words(cleanedText)
+        textWords = words(cleanedText).items()
         for word in textWords:
             corpusTokens += word[1]
-            if word[0] in corpusDict:
-                corpusDict[word[0]] += word[1]
+            if word[0] not in corpusList:
+                corpusList.append(word[0])
+    corpusList.sort()
+    
+    #Now, look at each text and get word frequencies (not the most efficient way to do this,
+    # but easy to implement)
+    occurrences = []
+    for textFile in textFiles:
+        occurrenceList = []
+        thisFile = open(textFile)
+        thisText = thisFile.read().lower()
+        thisFile.close()
+        cleanedText = re.sub(r"[^a-zA-Z0-9]", " ", thisText)
+        textWords = words(cleanedText)
+        for word in corpusList:
+            if word in textWords:
+                occurrenceList.append(textWords[word])
             else:
-                corpusDict[word[0]] = word[1]
+                occurrenceList.append(0)
+        occurrenceTuple = (textFile, occurrenceList)
+        occurrences.append(occurrenceTuple)
+    
+    #Finally, we will make a new list consisting of all cosine similarity
+    #values.
+    cosineList = []
+    #comparisons only need be done once
+    for i in range(0,len(occurrences)):
+        for j in range(i, len(occurrences)):
+            array1, array2 = np.array(occurrences[i][1]), np.array(occurrences[j][1])
+            #get dot product and magnitude
+            thisDot = np.dot(array1, array2)
+            mag1, mag2 = np.linalg.norm(array1), np.linalg.norm(array2)
+            #cos = (a.b)/(|a||b|)
+            thisCos = thisDot/(mag1 * mag2)
+            thisComp = occurrences[i][0] + "\t" + occurrences[j][0] + "\t"
+            cosTuple = (thisComp, thisCos)
+            cosineList.append(cosTuple)
+            
+    print(cosineList)
+    cosineList.sort(key = lambda x: x[1], reverse=True)
+    for element in cosineList:
+        print(element[0],element[1])
+
         
-    print("Tokens: " + str(corpusTokens), "Types: " + str(len(corpusDict.keys())), "Lower: " + lower, "Upper: " + upper)
 
 
+    
 
-def words(text) -> list:
+    #Finally, print
+    print("Tokens: " + str(corpusTokens), "Types: " + str(len(corpusList)), "Lower: " + lower, "Upper: " + upper)
+
+
+#function to get word frequency in a given text
+def words(text) -> dict:
     wordDict = {}
     #Look at each word
     splitText = text.split()
@@ -51,7 +98,6 @@ def words(text) -> list:
     sortedWordDict = {}
     for y in sortedWordList:
         sortedWordDict[y]=wordDict[y]
-    sortedItems = list(sortedWordDict.items())
-    return sortedItems[::-1]
+    return sortedWordDict
 
 main()
